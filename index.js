f6df4f774a401f83a13e3969b6c9eb1d784574e4
Varts <mcvarc@gmail.com>
2019-05-08 01:13:31 +0300

    get, query params, get-параметры, query string

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-$.ajax('https://repetitora.net/api/JS/Images',
-{
-    success: function(data){
-        data.forEach(element => {
-            const image = document.createElement('img');
-            image.src = element.original;
-            document.querySelector('body').appendChild(image);
-        });
-    }
-});
-const resultBlock = document.querySelector("result");
+
+const resultBlock = document.querySelector("#result");
+const pageNumber = document.querySelector("#page-number");
 const clickMeButton = document.querySelector("#click-me");
 clickMeButton.addEventListener("click", makeRequest);
+
 function makeRequest (){
-    resultBlock.innerHTML = 'result';
+    $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber.value}&count=1`, {
+        success: function(data){
+            data.forEach(element => {
+                const image = document.createElement('img');
+                image.src = element.original;
+                document.querySelector('#result').appendChild(image);
+            });
+        }
+    }); 
 }

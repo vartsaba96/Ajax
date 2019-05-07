38f2c99da1d4a6d051239af7a42c1daacf37e6d7
Varts <mcvarc@gmail.com>
2019-05-07 21:11:22 +0300

    Success, callback

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-$.ajax('http://httpbin.org/#/Images/get_image');
-console.log('Test requeast');
+$.ajax('https://repetitora.net/api/JS/Images',
+{
+    success: function(data){
+        data.forEach(element => {
+            const image = document.createElement('img');
+            image.src = element.original;
+            document.querySelector('body').appendChild(image);
+        });
+    }
+});
+const resultBlock = document.querySelector("result");
+const clickMeButton = document.querySelector("#click-me");
+clickMeButton.addEventListener("click", makeRequest);
+function makeRequest (){
+    resultBlock.innerHTML = 'result';
+}
